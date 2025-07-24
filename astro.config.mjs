e1bfbdb399c518664e52af6616d86d9fc3dbb7ff
Maudlin Oxalis <maudlin.oxalis@gmail.com>
2025-07-23 21:44:40 -0600

    oops forgot that part

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  integrations: [react()],
   output: 'static',
   base: '/dev-site-25/',
 });
